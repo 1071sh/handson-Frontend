fe88bdfeea654bef53315654d44faa0268a0e706
1071sh <i9i9ksy@gmail.com>
2021-12-05 20:59:27 +0900

    lesson6

---
+++ lesson6/index.js
@@ -0,0 +1,30 @@
+(function () {
+  const div = document.querySelector("div"),
+    ul = document.createElement("ul");
+  const promise = new Promise((resolve) => {
+    resolve([
+      { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+      { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+    ]);
+  });
+
+  const createHtml = (data) => {
+    data.map((item) => {
+      const li = document.createElement("li"),
+        img = document.createElement("img"),
+        link = document.createElement("a");
+      link.href = item.to;
+      img.src = item.img;
+      img.alt = item.alt;
+      li.textContent = item.text;
+      ul.appendChild(li).appendChild(link).appendChild(img);
+      div.appendChild(ul);
+    });
+  };
+
+  promise.then((data) => {
+    setTimeout(() => {
+      createHtml(data);
+    }, 3000);
+  });
+})();
