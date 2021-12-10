c4c653304dd7af606a36645e19ca9121704e9c34
1071sh <i9i9ksy@gmail.com>
2021-12-11 00:36:18 +0900

    lesson10

---
+++ lesson10/index.js
@@ -0,0 +1,57 @@
+(function () {
+  const div = document.querySelector("div"),
+    ul = document.createElement("ul"),
+    addDiv = document.createElement("div"),
+    loadImg = new Image(),
+    data = [
+      { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+      { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+    ];
+
+  loadImg.src = "loading-circle.gif";
+  addDiv.setAttribute("id", "loading");
+
+  const resolveFuc = (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        hideLoading();
+        resolve(createHtml(data));
+      }, 3000);
+    });
+  };
+
+  const createHtml = (data) => {
+    data.map((item) => {
+      const li = document.createElement("li"),
+        img = document.createElement("img"),
+        link = document.createElement("a");
+      link.href = item.to;
+      img.src = item.img;
+      img.alt = item.alt;
+      li.textContent = item.text;
+      ul.appendChild(li).appendChild(link).appendChild(img);
+      div.appendChild(ul);
+    });
+  };
+
+  const showLoading = () => {
+    document.body.appendChild(addDiv).appendChild(loadImg);
+  };
+
+  const hideLoading = () => {
+    document.body.removeChild(addDiv);
+  };
+
+  try {
+    async function finish() {
+      showLoading();
+      return await resolveFuc(data);
+    }
+    finish();
+  } catch (e) {
+    alert("エラーが発生しました。");
+    console.log(e);
+  } finally {
+    console.log("Finally!");
+  }
+})();
